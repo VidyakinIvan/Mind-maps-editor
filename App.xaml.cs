84f415c29f24d06562bca74f123038feebf1fd95
VidyakinIvan <D.V4an@yandex>
2024-03-27 11:00:31 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Mind_maps_editor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
