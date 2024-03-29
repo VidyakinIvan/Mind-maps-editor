59cd2ae305c2585f1ccc704452cf3e124ae29ff4
VidyakinIvan <D.V4an@yandex>
2024-03-30 00:21:46 +0300

    Add basic graph, node objects and nodeClicked event from  MSAGL

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,7 +8,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Microsoft.Msagl;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+using System.Windows.Forms;
+using System.Diagnostics;
 namespace Mind_maps_editor
 {
     /// <summary>
@@ -19,6 +23,22 @@ namespace Mind_maps_editor
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Graph graph = new();
+            graph.AddNode("A");
+            this.gViewer.Graph = graph;
+        }
+        private void GraphNodeClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("GraphNodeClicked");
+            if (sender is not GViewer gViewer || gViewer?.SelectedObject is not Node node)
+            {
+                return;
+            }
+            Debug.WriteLine(node.Label.Text);
+            System.Windows.MessageBox.Show(node.Label.Text);
         }
     }
 }
